b5c5c3b828b81c19431d5901cf965c3e54f00b39
vahidGhadiri <vahid.ghadiri1990@gmail.com>
2021-08-20 14:57:45 +0430

    online-transpiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from "react"
+import * as esbuild from "esbuild-wasm"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App: React.FC = () => {
+
+    const [input, setInput] = useState("")
+    const [code, setCode] = useState(" ")
+    const esRef = useRef<any>()
+
+    useEffect(() => {
+        startService()
+    }, [])
+
+    const startService = async () => {
+        esRef.current = await esbuild.startService({
+            worker: true,
+            wasmURL: "esbuild.wasm"
+        })
+    }
+
+    const handleOnClick = async () => {
+        if (!esRef.current) return
+
+        const result = await esRef.current.transform(input, {
+            loader: "jsx",
+            target: "es2015"
+        })
+        setCode(result.code)
+    }
+
+    return (
+        <div>
+            <textarea onChange={e => setInput(e.target.value)} value={input}/>
+            <div>
+                <button onClick={handleOnClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    )
 }
 
-export default App;
+export default App
