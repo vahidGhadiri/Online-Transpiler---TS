2d6c7bec52974dae63d4a47f956ea3f41c2cc47f
vahidGhadiri <vahid.ghadiri1990@gmail.com>
2021-08-21 16:12:27 +0430

    [Feature] - Change bundler

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -5,11 +5,12 @@ export const unpkgPathPlugin = () => {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
             build.onResolve({filter: /.*/}, async (args: any) => {
+                console.log("VahidResolve", args)
                 return {path: args.path, namespace: 'a'};
             });
 
             build.onLoad({filter: /.*/}, async (args: any) => {
-
+                console.log("VahidOnLoad", args)
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
