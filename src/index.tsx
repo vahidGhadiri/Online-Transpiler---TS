22e698a1a7c79dcbed96f12f0d7ff8ed399ee3a2
vahidGhadiri <whydrf@gmail.com>
2021-08-26 12:42:56 +0430

    [Feature] - setting localForage

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import * as esbuild from 'esbuild-wasm';
+import {unpkgPathPlugin} from './plugins/unpkg-path-plugin';
+
+const App = () => {
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm',
+        });
+    };
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window',
+            },
+        });
+
+        setCode(result.outputFiles[0].text);
+    };
+
+    return (
+        <div>
+      <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+      />
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,28 +1,37 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+    name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
             build.onResolve({filter: /.*/}, async (args: any) => {
+                console.log('onResolve', args);
                 if (args.path === 'index.js') {
                     return {path: args.path, namespace: 'a'};
                 }
 
-                if (args.path.includes("./") || args.path.includes("../")) {
+                if (args.path.includes('./') || args.path.includes('../')) {
                     return {
-                        namespace: "a",
-                        path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/").href
-                    }
+                        namespace: 'a',
+                        path: new URL(
+                            args.path,
+                            'https://unpkg.com' + args.resolveDir + '/'
+                        ).href,
+                    };
                 }
 
                 return {
-                    namespace: "a",
-                    path: `https://unpkg.com/${args.path}`
-                }
-
+                    namespace: 'a',
+                    path: `https://unpkg.com/${args.path}`,
+                };
             });
+
             build.onLoad({filter: /.*/}, async (args: any) => {
                 console.log('onLoad', args);
 
@@ -30,17 +39,27 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-              const message = require('medium-test-pkg');
-              console.log(message);
+              import React, { useState } from 'react-select';
+              console.log(React, useState);
             `,
                     };
                 }
+
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+                    args.path
+                );
+
+                if (cachedResult) return cachedResult;
                 const {data, request} = await axios.get(args.path);
-                return {
+
+                const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
-                    resolveDir: new URL(".", request.responseURL).pathname
+                    resolveDir: new URL('./', request.responseURL).pathname,
                 };
+                await fileCache.setItem(args.path, result);
+
+                return result;
             });
         },
     };
